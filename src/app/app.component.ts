88b861ff27a408a32ce6a09114ab4a0cc47542eb
Técnico em Informática 2018.2 <39121742018.2@senacrjedu.local>
2019-10-23 11:13:00 -0300

    inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,9 +17,9 @@ export class AppComponent {
       icon: 'home'
     },
     {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
+      title: 'Map',
+      url: '/map',
+      icon: 'people'
     }
   ];
 
